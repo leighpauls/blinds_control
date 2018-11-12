cdb7d278c52ef8d5c4722cac6ec1458a28dad91e
Leigh Pauls <leighpauls@fb.com>
2018-11-11 17:05:33 -0800

    All 4 blinds working

--- blinds_control.ino
+++ blinds_control.ino
@@ -1,7 +1,7 @@
 
 
-int UP_COMMAND_INPUT = 10;
-int DOWN_COMMAND_INPUT = 11;
+int UP_COMMAND_INPUT = A1;
+int DOWN_COMMAND_INPUT = A0;
 int BLINK_OUTPUT = 13;
 
 int NEUTRAL_DIRECTION = 0;
@@ -21,11 +21,19 @@ void setupBlind(BlindInfo* blind);
 void updateBlind(BlindInfo* blind);
 
 BlindInfo leftBlind = {
-  0, 1, 2, 3, NEUTRAL_DIRECTION,
+  1, 0, 4, 5, NEUTRAL_DIRECTION,
 };
 
 BlindInfo leftMiddleBlind = {
-  4, 5, 6, 7, NEUTRAL_DIRECTION,
+  3, 2, 6, 7, NEUTRAL_DIRECTION,
+};
+
+BlindInfo rightMiddleBlind = {
+  A3, A2, 8, 9, NEUTRAL_DIRECTION,
+};
+
+BlindInfo rightBlind = {
+  A5, A4, 10, 11, NEUTRAL_DIRECTION,
 };
 
 void setupBlind(BlindInfo* blind) {
@@ -34,15 +42,14 @@ void setupBlind(BlindInfo* blind) {
   digitalWrite(blind->upMotor, LOW);
   digitalWrite(blind->downMotor, LOW);
   
-  pinMode(blind->topSensor, INPUT);
-  pinMode(blind->bottomSensor, INPUT);
+  pinMode(blind->topSensor, INPUT_PULLUP);
+  pinMode(blind->bottomSensor, INPUT_PULLUP);
 }
 
 void updateBlind(BlindInfo* blind) {
-
-  if (digitalRead(UP_COMMAND_INPUT) == HIGH) {
+  if (digitalRead(UP_COMMAND_INPUT) == LOW) {
     blind->desiredDirection = UP_DIRECTION;
-  } else if (digitalRead(DOWN_COMMAND_INPUT) == HIGH) {
+  } else if (digitalRead(DOWN_COMMAND_INPUT) == LOW) {
     blind->desiredDirection = DOWN_DIRECTION;
   }
 
@@ -75,17 +82,21 @@ void updateBlind(BlindInfo* blind) {
 
 void setup() {
   delay(1000);
-  pinMode(UP_COMMAND_INPUT, INPUT);
-  pinMode(DOWN_COMMAND_INPUT, INPUT);
+  pinMode(UP_COMMAND_INPUT, INPUT_PULLUP);
+  pinMode(DOWN_COMMAND_INPUT, INPUT_PULLUP);
   pinMode(BLINK_OUTPUT, OUTPUT);
 
   setupBlind(&leftBlind);
   setupBlind(&leftMiddleBlind);
+  setupBlind(&rightBlind);
+  setupBlind(&rightMiddleBlind);
 }
 
 void loop() {
   updateBlind(&leftBlind);
   updateBlind(&leftMiddleBlind);
+  updateBlind(&rightBlind);
+  updateBlind(&rightMiddleBlind);
   digitalWrite(BLINK_OUTPUT, ((millis()/500) % 2) ? LOW : HIGH);
   delay(20);
 }
